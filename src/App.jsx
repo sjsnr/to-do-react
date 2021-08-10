0950a5a426d754f7b0fe64ee4244e379786d17a2
sjsnr <32189427+sjsnr@users.noreply.github.com>
2021-08-10 12:03:12 +0900

    [add] make function delete task

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,12 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -32,12 +38,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="area-title">未完了のTODO</p>
         <ul className="list-row">
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <li key="{todo">
                 <span>{todo}</span>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </li>
             );
           })}
