d73a815b7fdbfe50925ac56167e6a4a79df4b3f3
sjsnr <32189427+sjsnr@users.noreply.github.com>
2021-08-10 11:27:06 +0900

    [add] make appearance

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,31 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input type="text" placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="area-title">未完了のTODO</p>
+        <ul className="list-row">
+          <li>
+            <span>aaa</span>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="area-title">完了のTODO</p>
+        <ul className="list-row">
+          <li>
+            <span>aaa</span>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
